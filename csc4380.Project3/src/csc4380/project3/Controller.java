86111e40067c48d06531c94bb32609483c3a727b
joanaibarra <joana97.ji@gmail.com>
2018-11-26 15:46:41 -0500

    initialize listener to show seats

--- csc4380.Project3/src/csc4380/project3/Controller.java
+++ csc4380.Project3/src/csc4380/project3/Controller.java
@@ -26,6 +26,7 @@ public class Controller{
         views = view;
         views.addALSeats(new seatListener());
         views.addALMovie(new movieListener());
+        views.addTimeAL(new timeButtonsListener());
         models = model;
         //model.getConnection();
         
@@ -49,6 +50,12 @@ public class Controller{
             views.addTimes(models.getShowtimes(movieId)); 
             //how to populate this view with the data retrieved above
             views.showTimes();
+        }
+    }
+    
+    class timeButtonsListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            views.showSeats();
         }
     }
 }
